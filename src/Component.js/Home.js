9f57ae6603237eab004e791329a14409e6c5ab7a
Christopher <christopher.kim43@gmail.com>
2022-04-22 12:49:05 -0400

    Add image and title

--- src/Component.js/Home.js
+++ src/Component.js/Home.js
@@ -1,6 +1,8 @@
+import { ImageList } from '@mui/material'
 import React from 'react'
 import {useState, useEffect} from 'react'
 import { PieChart } from 'react-minimal-pie-chart'
+import logo from '../images/moneybags.png'
 
 function Home({wallet, budget, users, setUsers}) {
   
@@ -8,6 +10,7 @@ function Home({wallet, budget, users, setUsers}) {
   return (
     <>
       <div><h1 id='title'>Budget Tracker</h1></div>
+      <img id='cover-image' src={logo} />
     
      
     
